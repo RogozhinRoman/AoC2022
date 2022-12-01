955116c01121f19a54d1ea475d4d42f586d727ff
roman.r <roman.r@miro.com>
2022-12-01 14:11:54 +0400

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getGroups(input: List<String>): List<Int> {
+        val groups = mutableListOf(0)
+        var currentIndex = 0
+
+        for (line in input) {
+            if (line.isBlank()) {
+                currentIndex++
+                groups.add(0)
+            } else {
+                groups[currentIndex] += line.toInt()
+            }
+        }
+
+        return groups
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = getGroups(input).max()
+    fun part2(input: List<String>): Int = getGroups(input).sortedDescending().take(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
