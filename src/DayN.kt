11c47c09540ee46d964953ececb8d813e6645b30
roman.r <roman.r@miro.com>
2022-12-04 11:15:25 +0400

    Templates

---
+++ src/DayN.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //fun part1(input: List<String>) =
+
+    //fun part2(input: List<String>) =
+
+    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("DayN_test")
+//    check(part1(testInput) == 157)
+//    check(part2(testInput) == 70)
+//
+//    val input = readInput("DayN")
+//    println(part1(input))
+//    println(part2(input))
+}
