d978826a45a74595d2df091b383a33a84058ab8c
roman.r <roman.r@miro.com>
2022-12-05 13:32:55 +0400

    Day 5 solution

--- src/Day05.kt
+++ src/Day05.kt
@@ -16,14 +16,43 @@ fun main() {
     }
 
     fun getCommands(input: List<String>): List<Triple<Int, Int, Int>> {
-        val commands = input.dropWhile { !it.startsWith("move")}
+        val commands = input.dropWhile { !it.startsWith("move") }
         return commands.map {
             val splitted = it.split("move ", " from ", " to ")
             Triple(splitted[1].toInt(), splitted[2].toInt(), splitted[3].toInt())
         }
     }
 
-    fun part1(input: List<String>): String {
+    fun transfer1(
+        command: Triple<Int, Int, Int>,
+        from: ArrayDeque<String>,
+        to: ArrayDeque<String>
+    ) {
+        for (i in 0 until command.first) {
+            val elem = from.removeLast()
+            to.addLast(elem)
+        }
+    }
+
+    fun transfer2(
+        command: Triple<Int, Int, Int>,
+        from: ArrayDeque<String>,
+        to: ArrayDeque<String>
+    ) {
+        val toTransfer = ArrayDeque<String>()
+
+        for (i in 0 until command.first) {
+            toTransfer.addLast(from.removeLast())
+        }
+        for (i in 0 until command.first) {
+            to.addLast(toTransfer.removeLast())
+        }
+    }
+
+    fun handleTransfers(
+        input: List<String>,
+        transferFunc: (command: Triple<Int, Int, Int>, from: ArrayDeque<String>, to: ArrayDeque<String>) -> Unit
+    ): String {
         val crates = getCrates(input)
         val commands = getCommands(input)
 
@@ -31,34 +60,14 @@ fun main() {
             val from = crates[command.second - 1]
             val to = crates[command.third - 1]
 
-            for (i in 0 until command.first) {
-                val elem = from.removeLast()
-                to.addLast(elem)
-            }
+            transferFunc(command, from, to)
         }
 
         return crates.joinToString("") { it.last() }
     }
 
-    fun part2(input: List<String>): String {
-        val crates = getCrates(input)
-        val commands = getCommands(input)
-
-        for (command in commands) {
-            val from = crates[command.second - 1]
-            val to = crates[command.third - 1]
-            val toTransfer = ArrayDeque<String>()
-
-            for (i in 0 until command.first) {
-                toTransfer.addLast(from.removeLast())
-            }
-            for (i in 0 until command.first) {
-                to.addLast(toTransfer.removeLast())
-            }
-        }
-
-        return crates.joinToString("") { it.last() }
-    }
+    fun part1(input: List<String>) = handleTransfers(input, ::transfer1)
+    fun part2(input: List<String>) = handleTransfers(input, ::transfer2)
 
     val testInput = readInput("Day05_test")
     check(part1(testInput) == "CMZ")
