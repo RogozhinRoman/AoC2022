5cf9309dc9235d379c6edfee061991c88f9f0609
roman.r <roman.r@miro.com>
2022-12-06 11:43:58 +0400

    Day 6 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,18 +2,13 @@ fun main() {
     fun getChunkStart(input: String, limit: Int): Int {
         val buffer = ArrayDeque<Char>()
         for (fast in input.indices) {
-            if (fast == 0) {
-                buffer.addLast(input[fast])
-                continue
-            }
-            if (buffer.contains(input[fast])) {
+            if (fast != 0 && buffer.contains(input[fast])) {
                 do {
                     val first = buffer.removeFirst()
                 } while (first != input[fast])
-                buffer.addLast(input[fast])
-            } else {
-                buffer.addLast(input[fast])
             }
+
+            buffer.addLast(input[fast])
             if (buffer.size == limit) {
                 return fast + 1
             }
