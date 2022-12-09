c1ed2749457aa028096d48cfa093e94d65e0e234
roman.r <roman.r@miro.com>
2022-12-10 00:00:37 +0400

    Day 9 solution

--- src/Day09.kt
+++ src/Day09.kt
@@ -34,11 +34,7 @@ fun main() {
             return 0
         }
 
-        if (x1 > x2) {
-            return 1
-        }
-
-        return -1
+        return if (x1 > x2) 1 else -1
     }
 
     fun performStep(
